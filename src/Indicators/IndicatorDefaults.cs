d6b46ac7d6d3d5e70a519902c41e49bd1d933abe
Dylan Bienenstock <db@dylanbienenstock.com>
2019-05-03 14:15:55 -0700

    Add IndicatorDefaults

---
+++ src/Indicators/IndicatorDefaults.cs
@@ -0,0 +1,9 @@
+namespace CryptoBot.Indicators
+{
+    public static class IndicatorDefaults
+    {
+        public const int PeriodDuration = DebugConst.Timescale;
+        public const TradingPeriodAspect Aspect = TradingPeriodAspect.Close;
+        public const float Width = 2.5f;
+    }
+}
