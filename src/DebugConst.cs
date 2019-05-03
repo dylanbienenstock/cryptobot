bad7c3ab7bb5019c874b3c7f0f542b19700d4db6
Dylan Bienenstock <db@dylanbienenstock.com>
2019-05-03 14:09:46 -0700

    Add DebugConst

---
+++ src/DebugConst.cs
@@ -0,0 +1,10 @@
+namespace CryptoBot
+{
+    public static class DebugConst
+    {
+        public const int Timescale = 60000;  // 1m
+        // public const int Timescale = 180000; // 3m
+        // public const int Timescale = 300000; // 5m
+        // public const int Timescale = 900000; // 15m
+    }
+}
