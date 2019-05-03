f89049f2c281aceb855cefa48565f67cf2a842a5
Dylan Bienenstock <db@dylanbienenstock.com>
2019-05-03 14:17:43 -0700

    Add IndicatorColor

---
+++ src/Indicators/Renderers/IndicatorColor.cs
@@ -0,0 +1,11 @@
+using System.Drawing;
+
+namespace CryptoBot.Indicators.Renderers
+{
+    public static class IndicatorColor
+    {
+        public static Color Bullish = Color.FromArgb(255, 112, 168, 0); 
+        public static Color Bearish = Color.FromArgb(255, 235, 0, 112); 
+        public static Color Neutral = Color.FromArgb(255, 115, 115, 115); 
+    }
+}
