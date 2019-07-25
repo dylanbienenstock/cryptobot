9bbb70f996121930148e82865d8207d6bdac1dc5
Dylan Bienenstock <db@dylanbienenstock.com>
2019-07-25 00:41:54 -0700

    Add bool Collecting to CompletionTuple

--- src/Storage/CompletionTuple.cs
+++ src/Storage/CompletionTuple.cs
@@ -4,11 +4,18 @@ namespace CryptoBot.Storage
     {
         public double CompleteDays;
         public double TotalDays;
+        public bool   Collecting;
 
-        public CompletionTuple(double completeDays, double totalDays)
+        public CompletionTuple
+        (
+            double completeDays,
+            double totalDays,
+            bool collecting
+        )
         {
             CompleteDays = completeDays;
-            TotalDays = totalDays;
+            TotalDays    = totalDays;
+            Collecting   = collecting;
         }
     }
 }
